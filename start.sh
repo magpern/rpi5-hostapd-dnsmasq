cc15ae95bdab12bcaaf8a250bcd863a6af9ea7bc
Magnus Pernemark <magnus.pernemark@gmail.com>
2025-03-03 00:10:57 +0100

    Refactors IP assignment logic in start.sh
    Ensures wlan0 is UP before configuring IP
    Only adds IP if it is not already assigned

--- start.sh
+++ start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Set up the network interface
+# Ensure wlan0 is UP before configuring
 ip link set wlan0 up
-ip addr add 192.168.4.1/24 dev wlan0
+
+# Check if IP is already assigned before adding
+if ! ip addr show wlan0 | grep -q "192.168.4.1"; then
+    ip addr add 192.168.4.1/24 dev wlan0
+else
+    echo "IP address already assigned. Skipping..."
+fi
 
 # Start hostapd (WiFi Access Point)
 hostapd -B /etc/hostapd/hostapd.conf
